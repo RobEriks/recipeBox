56b64681b1edef1f5a37c6346cf0d5c7a0d80dc4
RobEriks <Rob.Eriksson@outlook.com>
2025-01-06 21:52:27 +0100

    U

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
-      
+    <div className="min-h-screen p-8 bg-gray-500">
+      <h1 className="text-3xl font-bold mb-6">Recipe Box</h1>
     </div>
   );
 }
